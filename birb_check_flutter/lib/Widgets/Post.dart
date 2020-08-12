b491a4a16df357ad8e8ffa03643ca03ebe785659
Joseph Johnson <jwjbadger@gmail.com>
2020-08-12 06:47:43 -0500

    Add post viewer

--- birb_check_flutter/lib/Widgets/Post.dart
+++ birb_check_flutter/lib/Widgets/Post.dart
@@ -16,7 +16,79 @@ class _PostViewState extends State<PostView> {
 
   @override
   Widget build(BuildContext context) {
-    print(widget.post.title);
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          widget.post.title,
+          style: TextStyle(
+            color: Theme.of(context).accentColor,
+          ),
+        ),
+      ),
+      body: Column(
+        children: [
+          Container(
+            decoration: BoxDecoration(
+              color: Theme.of(context).primaryColor,
+              borderRadius: BorderRadius.circular(4),
+            ),
+            child: Column(
+              children: [
+                Text(
+                  widget.post.description,
+                  style: TextStyle(
+                    color: Theme.of(context).unselectedWidgetColor,
+                  ),
+                ),
+                Row(
+                  children: [
+                    InkWell(
+                      child: Container(
+                        child: Icon(
+                          Icons.keyboard_arrow_up,
+                          color: Theme.of(context).unselectedWidgetColor,
+                        ),
+                      ),
+                      onTap: () {},
+                    ),
+                    InkWell(
+                      child: Container(
+                        child: Icon(
+                          Icons.keyboard_arrow_down,
+                          color: Theme.of(context).unselectedWidgetColor,
+                        ),
+                      ),
+                      onTap: () {},
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 4),
+                      child: Text(
+                        'By: ${widget.post.author['name']}',
+                        style: TextStyle(
+                          fontSize: 11,
+                          color: Theme.of(context).unselectedWidgetColor,
+                          fontStyle: FontStyle.italic,
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 4),
+                      child: Text(
+                        '${widget.post.upvotes.length - widget.post.downvotes.length} Internet Point(s)',
+                        style: TextStyle(
+                          fontSize: 11,
+                          color: Theme.of(context).unselectedWidgetColor,
+                          fontStyle: FontStyle.italic,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
